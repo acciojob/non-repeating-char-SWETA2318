9bd97a2cae487610502c8588e9c3ff1abfb2ee8e
SWETA2318 <150820032+SWETA2318@users.noreply.github.com>
2023-12-27 12:11:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+  // Boş string kontrolü
+  if (str.length === 0) {
+    return null;
+  }
+
+  // Her karakterin sayısını tutan bir nesne oluştur
+  const charCount = {};
+
+  // Her karakterin sayısını saymak için string'i döngüye al
+  for (let char of str) {
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
+
+  // İlk non-tekrarlanan karakteri bulmak için tekrar string'i döngüye al
+  for (let char of str) {
+    if (charCount[char] === 1) {
+      return char;
+    }
+  }
+
+  // Eğer non-tekrarlanan karakter bulunamazsa null döndür
+  return null;
 }
+
+// Kullanıcıdan giriş al
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+
+// Sonucu alert kullanarak göster
+alert(firstNonRepeatedChar(input));
